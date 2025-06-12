503efc49615e5c44eb550fcf67a348232def1a4d
ChaseJ <145057536+Lehua18@users.noreply.github.com>
2025-06-11 20:35:49 -0500

    more json converting

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,6 +9,7 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 import java.util.Scanner;
+
 
 
 public class Main {
@@ -18,7 +21,7 @@ public class Main {
 
 
     }
-    public static JSONObject URLToJSON(String URLString){
+    public static JSONArray URLToJSON(String URLString){
         try {
             URI uri = URI.create(URLString);
             URL url = uri.toURL();
@@ -35,13 +38,21 @@ public class Main {
                 }
                 reader.close();
                 System.out.println(response.toString());
-                String newJSONString = response.substring(1, response.toString().length()-2);
+                String newJSONString = response.toString()/*.replace("\"","\\\"")*/;
+//            String anotherNewJSONString = newJSONString.substring(1, response.toString().length()-2);
+                System.out.println(newJSONString);
 
-                return new JSONObject(newJSONString);
+                try {
+                    JSONArray object = new JSONArray(newJSONString);
+                    System.out.println(object.toString());
+                    return object;
+                } catch (JSONException e) {
+                    System.out.println("JSON creation failed: "+e);
+                    return null;
+                }
 
             } else {
                 System.out.println("GET request failed. Response code: " + responseCode);
-
                 return null;
             }
 
